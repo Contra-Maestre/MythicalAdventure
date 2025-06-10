efeb6b667f35b4b3a62686c580af329603102a92
Contra-Maestre <federico.albornoz967@gmail.com>
2025-06-09 23:34:00 -0500

    Inicio de iluminacion

--- World/finde_nivel.gd
+++ World/finde_nivel.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 @export var nextLevel : String
+signal ChangeLevel
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		get_tree().change_scene_to_file(nextLevel)
+		emit_signal("ChangeLevel")
