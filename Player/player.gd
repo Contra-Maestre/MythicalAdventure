bee5525e3ad16fb787b3341d1e838b3e8450f037
Contra-Maestre <federico.albornoz967@gmail.com>
2025-06-11 18:25:13 -0500

    Primer State creado

--- Player/player.gd
+++ Player/player.gd
@@ -24,67 +24,67 @@ var estadoActual = estados.NORMAL
 
 func _ready() -> void:
 	$CanvasLayer/HPProgressBar.value = life
-	lightPlayer.enabled = false
-	PruebaAuto.player = self
-	particlePlayer.emitting = false
-	quitPopup.id_pressed.connect(quitSettings)
-
-func _physics_process(delta: float) -> void:
-	if estadoActual == estados.NORMAL:
-		NormalProces(delta)
-	
-
-func NormalProces(delta):
-	direction = Input.get_axis("ui_left", "ui_right")
-	velocity.x = direction * speed * delta
-	
-	if direction != 0:
-		aniPlayer.play("walk")
-	else :
-		aniPlayer.play("idle")
-	
-	spritePlayer.flip_h = direction < 0 if direction != 0 else spritePlayer.flip_h
-	
-	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
-		velocity.y -= jumpForce
-		particlePlayer.emitting = true
-		$AudioStreamPlayerJump.play()
-	
-	if !is_on_floor():
-		velocity.y += gravity
-		particlePlayer.emitting = false
-	move_and_slide()
-	
-
-func _process(delta: float) -> void:
-	for ray in raycastdmg.get_children():
-		if ray.is_colliding():
-			var colision = ray.get_collider()
-			if colision.is_in_group("enemigo"):
-				if colision.has_method("takeDmg"):
-					colision.takeDmg(damage)
-
-func actualizaInterfrazFrutas():
-	frutaLabel.text = str(PruebaAuto.fruit)
-
-func TakeDamage(dmg):
-	if estadoActual != estados.HURT:
-		life -= dmg
-		aniPlayer.play("hurt")
-		$AudioStreamPlayerHurt.play()
-		estadoActual = estados.HURT
-		if life <= 0:
-			Dead()
-
-func Dead():
-	get_tree().reload_current_scene()
-
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "hurt":
-		estadoActual = estados.NORMAL
-
-func quitSettings(id):
-	match (id):
-		0:
-			get_tree().quit()
+	#lightPlayer.enabled = false
+	#PruebaAuto.player = self
+	#particlePlayer.emitting = false
+	#quitPopup.id_pressed.connect(quitSettings)
+#
+#func _physics_process(delta: float) -> void:
+	#if estadoActual == estados.NORMAL:
+		#NormalProces(delta)
+	#
+#
+#func NormalProces(delta):
+	#direction = Input.get_axis("ui_left", "ui_right")
+	#velocity.x = direction * speed * delta
+	#
+	#if direction != 0:
+		#aniPlayer.play("walk")
+	#else :
+		#aniPlayer.play("idle")
+	#
+	#spritePlayer.flip_h = direction < 0 if direction != 0 else spritePlayer.flip_h
+	#
+	#if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+		#velocity.y -= jumpForce
+		#particlePlayer.emitting = true
+		#$AudioStreamPlayerJump.play()
+	#
+	#if !is_on_floor():
+		#velocity.y += gravity
+		#particlePlayer.emitting = false
+	#move_and_slide()
+	#
+#
+#func _process(delta: float) -> void:
+	#for ray in raycastdmg.get_children():
+		#if ray.is_colliding():
+			#var colision = ray.get_collider()
+			#if colision.is_in_group("enemigo"):
+				#if colision.has_method("takeDmg"):
+					#colision.takeDmg(damage)
+#
+#func actualizaInterfrazFrutas():
+	#frutaLabel.text = str(PruebaAuto.fruit)
+#
+#func TakeDamage(dmg):
+	#if estadoActual != estados.HURT:
+		#life -= dmg
+		#aniPlayer.play("hurt")
+		#$AudioStreamPlayerHurt.play()
+		#estadoActual = estados.HURT
+		#if life <= 0:
+			#Dead()
+#
+#func Dead():
+	#get_tree().reload_current_scene()
+#
+#
+#func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	#if anim_name == "hurt":
+		#estadoActual = estados.NORMAL
+#
+#func quitSettings(id):
+	#match (id):
+		#0:
+			#get_tree().quit()
--- Scripts/StateMachine/moving.gd
+++ Scripts/StateMachine/moving.gd
@@ -1,1 +1,8 @@
 extends State
+
+
+func StatePhysicsProcess(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	owner.velocity.x = direction * owner.speed
+	owner.move_and_slide()
