f7a986265b09561e6c19a5d52e11a37131614d09
Contra-Maestre <federico.albornoz967@gmail.com>
2025-06-14 23:03:21 -0500

    maquina de estados con match completo

--- Scripts/state_mobing_machine.gd
+++ Scripts/state_mobing_machine.gd
@@ -3,7 +3,7 @@ extends Node
 @onready var player: Player = self.owner
 
 
-enum STATE {IDLE, RUN, INAIR, FALL}
+enum STATE {IDLE, RUN, INAIR, FALL, MOVINGAIR, MOVINGFALL}
 var state = STATE.IDLE
 
 func _physics_process(delta: float) -> void:
@@ -35,11 +35,33 @@ func _physics_process(delta: float) -> void:
 			player.aniPlayer.play("jump")
 			player.particlePlayer.emitting = true
 			$"../AudioStreamPlayerJump".play()
-			if player.velocity.y > 0: state = STATE.FALL
 			
+			if !player.is_on_floor() and Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+				state = STATE.MOVINGAIR
+			if !player.is_on_floor(): state = STATE.FALL
+		STATE.MOVINGAIR:
+			player.direction = Input.get_axis("ui_left", "ui_right")
+			player.velocity.x = player.direction * player.speed * delta
+			player.velocity.y += player.gravity
+			player.spritePlayer.flip_h = player.direction < 0 if player.direction != 0 else player.spritePlayer.flip_h
+			
+			if !Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
+				state = STATE.INAIR
+			if !player.is_on_floor(): state = STATE.FALL
 		STATE.FALL:
 			player.velocity.y += player.gravity
 			player.particlePlayer.emitting = false
 			
+			if !player.is_on_floor() and Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+				state = STATE.MOVINGFALL
+			if player.is_on_floor(): state =STATE.IDLE
+		STATE.MOVINGFALL:
+			player.direction = Input.get_axis("ui_left", "ui_right")
+			player.velocity.x = player.direction * player.speed * delta
+			player.velocity.y += player.gravity
+			player.spritePlayer.flip_h = player.direction < 0 if player.direction != 0 else player.spritePlayer.flip_h
+			
+			if !Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
+				state = STATE.FALL
 			if player.is_on_floor(): state =STATE.IDLE
 	player.move_and_slide()
